c9971e7f23a4492a6d02a26ed37da2c4e565d666
Zachary McNulty <zach.mcnulty1992@gmail.com>
2018-04-07 13:00:42 -0400

    added hook to start ui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate gtk;
+#[macro_use]
+extern crate relm;
+#[macro_use]
+extern crate relm_derive;
+
+mod window;
+mod provider;
+
 fn main() {
-    println!("Hello, world!");
+    window::start()
 }
